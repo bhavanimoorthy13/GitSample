8869f0d3b88372c7777612bbb03939b4f0e5e3ed
bhavanimoorthy13 <bhavanimoorthy2000@gmail.com>
2022-01-28 10:42:42 +0530

    pavi finished

--- src/org/git/SampleGit.java
+++ src/org/git/SampleGit.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class SampleGit {
+	private void m2() {
+		System.out.println("pavi");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Done..");
 	}
