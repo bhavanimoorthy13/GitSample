3ead7f268fcfa0b625a3c9f7c1cb2d30b2f10254
bhavanimoorthy13 <bhavanimoorthy2000@gmail.com>
2022-01-28 10:59:28 +0530

    finished

--- src/org/git/SampleGit.java
+++ src/org/git/SampleGit.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class SampleGit {
+	private void m3() {
+		System.out.println("priya");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Done..");
 	}
